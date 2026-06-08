546cdeba0ef3634ab2a1a2c630f87240d782faaa
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-06-08 14:20:39 +0200

    second try at task 0, added a line to the end of the file to make sure it is not empty, and added a new line at the end of the file to make sure it ends with a new line, as per PEP 8 guidelines.

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-
 """
-this script lists all stats from the database hbtn_0e_0_usa
+This script lists all states from the database hbtn_0e_0_usa
 """
 import sys
-
 import MySQLdb
 
 if __name__ == "__main__":
-    """
-    this function lists all states from the database hbtn_0e_0_usa
-    """
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
     states = cursor.fetchall()
+    
     for state in states:
         print(state)
+        
     cursor.close()
     db.close()
