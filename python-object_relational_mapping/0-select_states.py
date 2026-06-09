60872d62d4a2bc65d2259aee48dee5b641d1e908
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-06-09 14:27:00 +0200

    fix task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,15 +10,16 @@ def main() -> None:
     password: str = sys.argv[2]
     db_name: str = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=user, passwd=password, db=db_name,
+    db = MySQLdb.connect(host="localhost", 
+                         port=3306,
+                         user=user, 
+                         passwd=password, 
+                         db=db_name,
                          charset="utf8")
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
     for row in cursor.fetchall():
-        state_id = row[0]
-        state_name = row[1]
-        print(f"{state_id}: {state_name}")
+        print(row)
     cursor.close()
     db.close()
 
