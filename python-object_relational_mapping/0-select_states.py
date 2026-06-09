b26e46a2b6768e8a7d385260a5832c132c35c405
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-06-09 14:28:55 +0200

    last try

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-"""List all states from the specified MySQL database."""
+"""Module to list all states from database"""
 import MySQLdb
 import sys
 
+if __name__ == "__main__":
 
-def main() -> None:
-    """Print all rows from the `states` table ordered by id."""
-    user: str = sys.argv[1]
-    password: str = sys.argv[2]
-    db_name: str = sys.argv[3]
+    """Lists all states sorted by id"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", 
-                         port=3306,
-                         user=user, 
-                         passwd=password, 
-                         db=db_name,
-                         charset="utf8")
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    for row in cursor.fetchall():
+    results = cursor.fetchall()
+
+    for row in results:
         print(row)
+
     cursor.close()
     db.close()
-
-
-if __name__ == "__main__":
-    main()
-
