d3ba0431c6197b4b10773d6197f855461b9cd370
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-06-30 11:14:51 +0200

    fixed variable not defined

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -3,8 +3,4 @@
 const first = parseInt(process.argv[2]);
 const second = parseInt(process.argv[3]);
 
-if (isNaN(first) || isNaN(second)) {
-  console.log('NaN');
-} else {
-  console.log(first + second);
-}
+console.log(first + second);
